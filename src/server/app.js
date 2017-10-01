c69ce6c40a0d6bd9a25d0fe9cb78850b54718c30
Unknown <msheldon83@gmail.com>
2017-10-01 00:27:19 -0400

    Proxy calls through app logic

--- src/server/app.js
+++ src/server/app.js
@@ -3,6 +3,30 @@ class App{
         this.repository = repository; 
         this.fireHose = fireHose;
     }
+
+    addConnection(conn, sid){
+        return repository.addConnection(conn, sid);
+    }
+
+    getQueries(sid){
+        return repository.getQueries(sid);
+    }
+
+    addQuery(query, sid){
+        let response = repository.addQuery(query.sid);
+        // TODO start firehose
+
+        return response;
+    }
+
+    removeQueryById(qid, sid){
+        let response = repository.removeQueryById(qid, sid);
+        // TODO stop firehose???
+        
+        return response;
+    }
+
+
 }
 
 export default App;
